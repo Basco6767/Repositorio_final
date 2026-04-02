0a4f5c36d396f52531cc89139deb2112ae86b03f
up202507000 <up202507000@up.pt>
2026-04-02 17:34:46 +0100

    Updated university_class.py

--- g12_project/university_class.py
+++ g12_project/university_class.py
@@ -2,6 +2,40 @@
 """
 Created on Thu Apr  2 17:07:46 2026
 
-@author: vasco
+@author: Sofia Teixeira
 """
+import Gclass
+class University(Gclass):
+  obj = dict()
+  lst = list()
+  pos = 0
+  sortkey = ''
+  att = ['uni_id','uni_name','foundation_date']
+  des = ['University Id','University Name', 'Foundation Date']
+
+def __init__(self, uni_id, uni_name, foundation_date):
+  super().__init__()
+  self._uni_id = int(uni_id)
+  self._uni_name = uni_name
+  self._foundation_date = foundation_date 
+
+  University.obj[self._uni_id] = self
+  University.lst.append(self._uni_id)
+
+@property
+def uni_id(self):
+  return int(self._uni_id)
+
+@property
+def uni_name(self):
+  return self._uni_name
+
+@property
+def foundation_date(self):
+  return self._foundation_date
+
+
+
+
+  
 
