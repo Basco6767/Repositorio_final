f3e18e5752a3ab7afbb6da9fe3b264da4cdd1ace
taniatavaress <up202506471@up.pt>
2026-04-02 17:34:59 +0100

    Update graduate_class.py

--- g12_project/graduate_class.py
+++ g12_project/graduate_class.py
@@ -4,4 +4,19 @@ Created on Thu Apr  2 17:09:19 2026
 
 @author: vasco
 """
+class Graduate:
+    def __init__(self, graduate_id, university_id, observations):
+        self._graduate_id = graduate_id
+        self._university_id = university_id
+        self.observations = observations
 
+    @property
+    def graduate_id(self):
+        return self._graduate_id
+
+    @property
+    def university_id(self):
+        return self._university_id
+
+    def __str__(self):
+        return f"Graduate(ID: {self._graduate_id}, Univ ID: {self._university_id}, Obs: {self.observations})"
