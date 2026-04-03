1b5400d91926eaddf97fb0f6fb750f21c45cab2d
mariaspteixeira08 <up202505257@g.uporto.pt>
2026-04-03 11:34:27 +0100

    Update association_class.py

--- g12_project/association_class.py
+++ g12_project/association_class.py
@@ -2,6 +2,32 @@
 """
 Created on Thu Apr  2 17:07:56 2026
 
-@author: vasco
+@author: Maria
 """
+import Gclass
+class Association(Gclass):
+  obj=dict()
+  lst=list()
+  pos=0
+  sortkey=""
+  att=["_association_id","_objective","_designation"]
+  des=["Association ID","Objective","Designation"]
+  def __init__(self, association_id, objective, designation):
+    super().__init__()
+    self._association_id=int(association_id)
+    self._objective=objective
+    self._designation=designation
+    Association.obj[self._association_id]=self
+    Association.lst.append(self._association_id)
 
+  @property
+  def association_id(self):
+    return self._association_id
+  @property
+  def objective(self):
+    return self._objective
+  @property
+  def designation(self):
+    return self._designation
+  def __str__(self):
+    return f"Association(ID: {self._association_id}, Designation: {self._designation}, Objective: {self._objective})"
