9d9ef2553cf368aaf31e737334ae8b823c47b8a8
taniatavaress <up202506471@up.pt>
2026-04-03 12:44:57 +0100

    Update graduate_class.py

--- g12_project/graduate_class.py
+++ g12_project/graduate_class.py
@@ -6,33 +6,29 @@ Created on Thu Apr  2 17:09:19 2026
 """
 import Gclass
 class Graduate(Gclass):
-  obj = dict()
-  lst = list()
-  pos = 0
-  sortkey = ''
-  att = ['graduate_id','university_id','observations']
-  des = ['Graduate ID','University ID', 'Observations']
+    obj = dict()
+    lst = list()
+    pos = 0
+    sortkey = ''
+    att = ['graduate_id','university_id','observations']
+    des = ['Graduate ID','University ID', 'Observations']
 
-  def __init__(self, graduate_id, university_id, observations): 
-      super().__init__()
-      self._graduate_id = graduate_id
-      self._university_id = university_id
-      self.observations = observations
+    def __init__(self, graduate_id, university_id, observations): 
+        super().__init__()
+        self._graduate_id = graduate_id
+        self._university_id = university_id
+        self.observations = observations 
+        
+        Graduate.obj[self._graduate_id] = self
+        Graduate.lst.append(self._graduate_id)
       
-      Graduate.obj[self._graduate_id] = self
-      Graduate.lst.append(self._graduate_id)
+    @property
+    def graduate_id(self):
+        return self._graduate_id
       
-      @property
-      def graduate_id(self):
-          return self._graduate_id
-      
-      @property
-      def university_id(self):
-          return self._university_id
-      
-      @property
-      def observations(self):
-          return self.observations
-      
-      def __str__(self):
-          return f"Graduate(ID: {self._graduate_id}, Univ ID: {self._university_id}, Obs: {self.observations})"
+    @property
+    def university_id(self):
+        return self._university_id
+    
+    def __str__(self):
+        return f"Graduate(ID: {self._graduate_id}, Univ ID: {self._university_id}, Obs: {self.observations})"
